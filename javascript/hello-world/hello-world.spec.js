4ebd834aeb35049b38eb61b3b15601aaa05fb655
sagar jadhav <jadhavsagar922@gmail.com>
2019-10-31 08:51:09 +0530

    Update hello-world.spec.js

--- javascript/hello-world/hello-world.spec.js
+++ javascript/hello-world/hello-world.spec.js
@@ -1,12 +1,12 @@
 var HelloWorld = require('./hello-world');
-
+//describing the functions
 describe('Hello World', function() {
   var helloWorld = new HelloWorld();
-
+//function having no name
   it('says hello world with no name', function() {
     expect(helloWorld.hello('')).toEqual('Hello, World!');
   });
-
+//bob is been named here
   it('says hello to bob', function() {
     expect(helloWorld.hello('Bob')).toEqual('Hello, Bob!');
   });
